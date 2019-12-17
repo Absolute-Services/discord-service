5fadc4d4344299e0ff4c353803887bdef957174e
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-12-17 13:28:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
+  client.user.setActivity("Made using Absolute Development's service")
   console.log(`Your discord bot is now online: ${client.user.name} | usercount: ${client.users.size} | channel count: ${client.channels.size} | guild count: ${client.guilds.size}`)
 })
 

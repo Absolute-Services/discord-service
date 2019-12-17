b7f837253a00a99ac38b911ff2307786af59bdf2
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-12-17 13:25:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const discord = require("discord.js")
+const discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+  console.log(`Your discord bot is now online: ${client.user.name} | usercount: ${client.users.size} | channel count: ${client.channels.size} | guild count: ${client.guilds.size}`)
+})
+
+client.login("Your token")
